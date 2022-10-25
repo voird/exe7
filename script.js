f120511e07de6cf37e148de87a740ab55c576b2b
polina_storozh <58592174+voird@users.noreply.github.com>
2022-10-25 23:39:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-  $(document).ready(function(){
-    $('.slider').slick({
-      arrows:true,
-      dots:true,
-      adaptiveHeight:true,
-      slidesToShow:4,
-      slidesToScroll:4,
-      speed: 1000,
-      responsive:[
-        {
-          breakpoint: 768,
-          settings:{
-            slidesToShow:2,
-            slidesToScroll:2
-          }
-        }
-      ]
+  window.addEventListener("DOMContentLoaded", function(event) {
+    $(document).ready(function(){
+        $('.slider').slick({
+          arrows:true,
+          dots:true,
+          adaptiveHeight:true,
+          slidesToShow:4,
+          slidesToScroll:4,
+          speed: 1000,
+          responsive:[
+            {
+              breakpoint: 768,
+              settings:{
+                slidesToShow:2,
+                slidesToScroll:2
+              }
+            }
+          ]
+        });
+      });
     });
-  });
